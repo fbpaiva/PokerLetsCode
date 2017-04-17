56b004179ce2b37fd3aea6b07748bae51fcbd68b
mac <mac@MacBook-Pro-de-mac.local>
2017-04-17 20:58:03 -0300

    Valores e Naipes válidos no Maco

--- src/Maco.java
+++ src/Maco.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
 
 public class Maco {
-	public String[] valoresDisponiveis = {};
-	public String[] naipesDisponiveis = {};
+	public String[] valoresDisponiveis = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+	public String[] naipesDisponiveis = {"P", "C", "E", "O"}; 
+	
+	ArrayList<Carta> maco = new ArrayList<>();
 	
 	public Maco(){
 		//inicializar um maço com todas as cartas disponível
+		
+		
 	}
 	
 	public void embaralhar(){
